2934cc6e00d0e7b213b46badf6b7ea7343405e8c
robrobbins <rob.robb.ns@gmail.com>
2019-07-23 08:55:08 -0700

    const is a thing

---
+++ src/util/constants.ts
@@ -0,0 +1,10 @@
+// can't use a ts Enum cuz the numbers don't go up in order (obv). 
+// NOTE: update as needed...
+export const NETWORKS = {
+  MAIN_NET: 1,
+  MORDEN: 2,
+  ROPSTEN: 3,
+  RINKEBY: 4,
+  KOVAN: 42,
+  SKYNET: 29458,
+}
