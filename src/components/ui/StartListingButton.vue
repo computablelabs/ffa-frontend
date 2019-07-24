bfac191271d8a97fbc946e9dd1b365b8836429dc
Melvin Mallari <mtmallari@ucdavis.edu>
2019-07-24 15:12:15 -0700

    Add Start Listing button, Generate flash confirmation of listing start

---
+++ src/components/ui/StartListingButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="button-container">
+    <button class="button" @click.prevent="startListing">Start Listing</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import FlashesModule from '../../modules/FlashesModule'
+  import { getModule } from 'vuex-module-decorators'
+  import Flash from '../../models/Flash'
+  import { FlashType } from '../../models/Flash'
+
+  export default Vue.extend({
+    methods: {
+      startListing: function () {
+        // console.log('Starting Listing...')
+        const flashesModule = getModule(FlashesModule, this.$store)
+        const flash = new Flash('Starting Listing', FlashType.info)
+        flashesModule.append(flash)
+      },
+    },
+  })
+</script>
+
+<style scoped lang='sass'>
+.button-container
+  display: flex
+  justify-content: center
+
+.button
+  border-radius: 20px
+  margin-top: 30px
+  display: flex
+  justify-content: flex-end
+  background-color: #eee
+  border: none
+
+.button:hover
+  background-color: #ddd
+</style>
