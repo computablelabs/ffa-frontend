b9ab11e517cadd15f6163d4379a4148aa5c1a777
Melvin Mallari <mtmallari@ucdavis.edu>
2019-08-07 10:22:23 -0700

    Add auto-lint prior to unit test

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,13 +2,13 @@ import { Nos } from '@computable/computablejs/dist/@types'
 import Web3 from 'web3'
 
 declare interface Transaction {
-  to?: string;
-  from?: string;
-  gas?: Nos;
-  gasPrice?: Nos;
-  nonce?: Nos;
-  data?: string;
-  value?: Nos;
+  to?: string
+  from?: string
+  gas?: Nos
+  gasPrice?: Nos
+  nonce?: Nos
+  data?: string
+  value?: Nos
 }
 
 declare interface RpcRequest {
@@ -18,21 +18,21 @@ declare interface RpcRequest {
 }
 
 declare interface RpcResponse {
-  jsonrpc: string;
-  error: any;
-  result: any;
-  id: any;
+  jsonrpc: string
+  error: any
+  result: any
+  id: any
 }
 
 declare global {
   namespace ethereum {
-    const isMetaMask: boolean;
-    let selectedAddress: string;
-    let networkVersion: string;
+    const isMetaMask: boolean
+    let selectedAddress: string
+    let networkVersion: string
 
-    function enable(): Promise<string[]>;
+    function enable(): Promise<string[]>
     // TODO make it possible to import Transaction from comp.js/...
-    function send(opts:Transaction): Promise<RpcResponse>;
-    function sendAsync(request: RpcRequest, callback: (err: any, res: any)=>void): void;
+    function send(opts: Transaction): Promise<RpcResponse>
+    function sendAsync(request: RpcRequest, callback: (err: any, res: any) => void): void
   }
 }
