5d0b7a1d3356d2611f7fb76735caa0db14649e66
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-08-16 00:14:29 +0800

    Fixes #127 (#137)

--- tests/unit/components/ui/FlashMessage.spec.ts
+++ tests/unit/components/ui/FlashMessage.spec.ts
@@ -10,8 +10,12 @@ import FileUploader from '@/components/listing/FileUploader.vue'
 import Flash, { FlashType } from '../../../../src/models/Flash'
 
 const localVue = createLocalVue()
+
 library.add(faFileSolid, faFile, faCheckCircle)
+
 const notificationClass = 'notification'
+const warningType = FlashType.warning
+const warningClass = 'is-warning'
 
 describe('FlashMessage.vue', () => {
 
@@ -30,17 +34,14 @@ describe('FlashMessage.vue', () => {
     expect(wrapper.findAll(`.${notificationClass}`).length).toBe(1)
   })
 
-  it('correctly renders Flash', () => {
-    const message = 'Specs are fun!'
-    const flash = new Flash(message, FlashType.info)
+  it('renders warning message', () => {
+    const message = 'a bleak warning from the future'
+    const flash = new Flash(message, warningType)
     const wrapper = shallowMount(FlashMessage, {
-      attachToDocument: true,
-      store: appStore,
-      localVue,
-      propsData: {
-        flash,
-      },
+      propsData: { flash },
     })
-    expect(wrapper.text()).toEqual('Specs are fun!')
+    expect(wrapper.findAll(`.${notificationClass}`).length).toBe(1)
+    expect(wrapper.findAll(`.${warningClass}`).length).toBe(1)
+    expect(wrapper.text()).toMatch(message)
   })
 })
