41b619a555087dfcb0e8d6b227156e706c7a4cb4
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-08-15 18:34:04 +0800

    Fixes #131

--- src/components/listing/FileLister.vue
+++ src/components/listing/FileLister.vue
@@ -6,17 +6,23 @@
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { NoCache } from 'vue-class-decorator'
 import { MutationPayload } from 'vuex'
+
 import { getModule } from 'vuex-module-decorators'
+import Web3Module from '../../vuexModules/Web3Module'
+import FlashesModule from '../../vuexModules/FlashesModule'
 import ListModule from '../../vuexModules/ListModule'
 import UploadModule from '../../vuexModules/UploadModule'
-import Web3Module from '../../vuexModules/Web3Module'
 import MetaMaskModule from '../../vuexModules/MetaMaskModule'
 import FfaListingsModule from '../../vuexModules/FfaListingsModule'
+
 import ListingModule from '../../functionModules/protocol/ListingModule'
+import FileListerModule from '../../functionModules/components/FileListerModule'
+
 import FfaListing from '../../models/FfaListing'
 import { ProcessStatus } from '../../models/ProcessStatus'
+
 import { Errors, Labels, Messages } from '../../util/Constants'
-import FileListerModule from '../../../src/functionModules/components/FileListerModule'
+
 import Web3 from 'web3'
 
 const vuexModuleName = 'listModule'
@@ -33,7 +39,11 @@ export default class FileLister extends Vue {
       case `${vuexModuleName}/setStatus`: {
         switch (mutation.payload) {
           case ProcessStatus.Executing:
-            FileListerModule.list()
+            const web3Module = getModule(Web3Module, this.$store)
+            const flashesModule = getModule(FlashesModule, this.$store)
+            const listModule = getModule(ListModule, this.$store)
+            const uploadModule = getModule(UploadModule, this.$store)
+            FileListerModule.list(web3Module, flashesModule, listModule, uploadModule)
             break
           default:
             break
--- src/functionModules/protocol/ListingModule.ts
+++ src/functionModules/protocol/ListingModule.ts
@@ -5,7 +5,10 @@ import { TransactOpts } from '@computable/computablejs/dist/interfaces'
 import Web3 from 'web3'
 
 import { getModule } from 'vuex-module-decorators'
+import Web3Module from '../../vuexModules/Web3Module'
 import FlashesModule from '../../vuexModules/FlashesModule'
+import ListModule from '../../vuexModules/ListModule'
+import UploadModule from '../../vuexModules/UploadModule'
 import store from '../../store'
 
 import ContractAddresses from '../../models/ContractAddresses'
@@ -26,20 +29,23 @@ export default class ListingModule {
     return listing
   }
 
-  public static async postListing(
-    account: string,
-    web3: Web3,
-    listingHash: string,
-    transactOpts: TransactOpts,
-    success: (response: any) => void) {
+  public static async postListing(account: string,
+                                  web3Module: Web3Module,
+                                  flashesModule: FlashesModule,
+                                  listModule: ListModule,
+                                  uploadModule: UploadModule,
+                                  transactOpts: TransactOpts,
+                                  success: (response: any,
+                                            flashesModule: FlashesModule,
+                                            listModule: ListModule,
+                                            uploadModule: UploadModule) => void) {
 
-    const flashesModule = getModule(FlashesModule, store)
-    flashesModule.append(new Flash(`listingHash: ${listingHash}`, FlashType.info))
-    const listing = await ListingModule.getListing(account, web3)
-    const method =  await listing.list(listingHash, transactOpts)
+    flashesModule.append(new Flash(`listingHash: ${listModule.listing.hash}`, FlashType.info))
+    const listing = await ListingModule.getListing(account, web3Module.web3)
+    const method =  await listing.list(listModule.listing.hash, transactOpts)
     // method[0] can be used to estimate the gas vs the abi generated figures
     const est = await method[0].estimateGas({from: account})
-    const unsigned = await buildTransaction(web3, method)
+    const unsigned = await buildTransaction(web3Module.web3, method)
     // build transaction adds `data` and `nonce` fields, but we still require `to` and `value`
     unsigned.to = ContractAddresses.ListingAddress
     unsigned.value = ZERO_HASHED
@@ -49,6 +55,6 @@ export default class ListingModule {
     if (est > unsigned.gas) {
       unsigned.gas = est
     }
-    send(web3, unsigned, flashesModule, success)
+    send(web3Module.web3, unsigned, flashesModule, listModule, uploadModule, success)
   }
 }
