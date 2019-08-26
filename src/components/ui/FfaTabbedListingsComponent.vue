199ba9eb7af992474f76f5b4a60de241aea20094
Melvin Mallari <mtmallari@ucdavis.edu>
2019-08-26 16:31:44 -0700

    Finish merging

--- src/components/ui/FfaTabbedListingsComponent.vue
+++ src/components/ui/FfaTabbedListingsComponent.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div class="columns-container">
     <FfaListingsTabs 
@@ -45,63 +44,3 @@ export default class FfaTabbedListingsComponent extends Vue {
   }
 }
 </script>
-||||||| merged common ancestors
-=======
-<template>
-  <div class="columns-container">
-    <div class="column is-3 tabs-container">
-      <button 
-        v-for="tab in tabs" 
-        class="tab-button"
-        :key="tab"
-        :class="{active: tab === selectedTab}"
-        @click="selectedTab = tab" >
-        {{tab}}
-      </button>
-    </div>
-    <FfaListingsComponent
-      :userAddress="userAddress"
-      :status="provideStatus()"/>
-  </div>
-</template>
-
-<script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator'
-import '@/assets/style/components/listing.sass'
-import FfaListing, { FfaListingStatus } from '../../models/FfaListing'
-import FfaListingsComponent from './FfaListingsComponent.vue'
-
-const allTab = 'all'
-const candidatesTab = 'candidates'
-const listedTab = 'listed'
-
-@Component({
-  components: {
-    FfaListingsComponent,
-  },
-})
-export default class FfaTabbedListingsComponent extends Vue {
-  public tabs: string[] = [allTab, candidatesTab, listedTab]
-  public selectedTab: string = 'all'
-
-  @Prop()
-  public userAddress?: string
-
-  @Prop()
-  public status?: FfaListingStatus
-
-  private provideStatus(): string {
-    switch (this.selectedTab) {
-      case allTab:
-        return ''
-      case candidatesTab:
-        return FfaListingStatus.candidate
-      case listedTab:
-        return FfaListingStatus.listed
-      default:
-        return ''
-    }
-  }
-}
-</script>
->>>>>>> 470d10ed7691237db154c491d4f33650e7854759
