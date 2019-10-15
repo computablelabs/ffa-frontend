43a7abfa77efa940136d30edec850b46aa3db1c8
Melvin Mallari <melvintmallari@gmail.com>
2019-10-15 11:37:36 -0700

    Add tabs to FfaListedView

--- src/views/FfaListedView.vue
+++ src/views/FfaListedView.vue
@@ -127,7 +127,6 @@ export default class FfaListedView extends Vue {
 
   @Prop({ default: false })
   public requiresParameters?: boolean
-
   public appModule: AppModule = getModule(AppModule, this.$store)
   public web3Module: Web3Module = getModule(Web3Module, this.$store)
   public flashesModule: FlashesModule = getModule(FlashesModule, this.$store)
