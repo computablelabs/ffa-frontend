e61d17f4c8054a39139cbaf379107446c5f071f6
Melvin Mallari <melvintmallari@gmail.com>
2019-10-17 10:59:32 -0700

    Add Challenge Drawer specs

--- tests/unit/views/drawers/ChallengeDrawer.spec.ts
+++ tests/unit/views/drawers/ChallengeDrawer.spec.ts
@@ -9,12 +9,17 @@ import FfaListingsModule from '../../../../src/vuexModules/FfaListingsModule'
 import ChallengeDrawer from '@/views/drawers/ChallengeDrawer.vue'
 
 import VotingModule from '../../../../src/vuexModules/VotingModule'
+import ChallengeModule from '../../../../src/vuexModules/ChallengeModule'
 import VueRouter from 'vue-router'
+import MarketTokenContractModule from '../../../../src/functionModules/protocol/MarketTokenContractModule'
+import { ChallengeStep } from '../../../../src/models/ChallengeStep'
 
+import flushPromises from 'flush-promises'
 // tslint:disable no-shadowed-variable
 
 
 let appModule!: AppModule
+let challengeModule!: ChallengeModule
 let web3Module!: Web3Module
 let ffaListingsModule!: FfaListingsModule
 let votingModule!: VotingModule
@@ -32,6 +37,7 @@ describe('VotingDrawer.vue', () => {
     web3Module = getModule(Web3Module, appStore)
     ffaListingsModule = getModule(FfaListingsModule, appStore)
     votingModule = getModule(VotingModule, appStore)
+    challengeModule = getModule(ChallengeModule, appStore)
 
   })
 
@@ -41,7 +47,7 @@ describe('VotingDrawer.vue', () => {
     }
   })
 
-  it('renders renders the challeng drawer view', () => {
+  it('renders renders the challenge drawer view', () => {
     wrapper = mount(ChallengeDrawer, {
       attachToDocument: true,
       store: appStore,
@@ -50,4 +56,48 @@ describe('VotingDrawer.vue', () => {
 
     expect(wrapper.find('.challenge-drawer-wrapper').exists()).toBeTruthy()
  })
+
+  it('renders renders the allow staking button conditionally', () => {
+      MarketTokenContractModule.getBalance = () => Promise.resolve('1000000000000000000')
+      MarketTokenContractModule.allowance = () => Promise.resolve('0')
+
+      wrapper = mount(ChallengeDrawer, {
+        attachToDocument: true,
+        store: appStore,
+        localVue,
+      })
+
+      setAppParams()
+      appModule.setAppReady(true)
+
+      // Should allow staking
+      expect(wrapper.findAll('.process-button').length).toBe(1)
+      expect(wrapper.find('.process-button').text()).toBe('Allow Staking')
+  })
+
+  it('renders the challenge stake button conditionally', async () => {
+      MarketTokenContractModule.getBalance = () => Promise.resolve('1000000000000000000')
+      MarketTokenContractModule.allowance = () => Promise.resolve('1000')
+      appModule.setAppReady(true)
+
+      wrapper = mount(ChallengeDrawer, {
+        attachToDocument: true,
+        store: appStore,
+        localVue,
+      })
+      await flushPromises()
+
+      expect(wrapper.findAll('.process-button').length).toBe(1)
+      expect(wrapper.find('.process-button').text()).toBe('Challenge Listing')
+  })
 })
+
+function setAppParams() {
+  appModule.setMakerPayment(1)
+  appModule.setCostPerByte(1)
+  appModule.setStake(1)
+  appModule.setPriceFloor(1)
+  appModule.setPlurality(1)
+  appModule.setVoteBy(1)
+  appModule.setDatatrustContractAllowance(1)
+}
