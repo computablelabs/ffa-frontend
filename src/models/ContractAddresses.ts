6e88a4423cb85554dad9e3258bbf87106dbdacb6
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-10-22 07:34:35 +0800

    Post audit contract and HOC changes

--- src/models/ContractAddresses.ts
+++ src/models/ContractAddresses.ts
@@ -1,9 +1,9 @@
 export default class ContractsAddresses {
-  public static EtherTokenAddress = '0xf3AB33fD326356e9aC525bfFc6865D42FdC237F0'
-  public static MarketTokenAddress = '0xfC29Ef8486f3a6F345d9e987Fd24D03C58e8d69B'
-  public static ListingAddress = '0xbEB3b451b589372d3b1A70d421B4B56EdDC0a654'
-  public static VotingAddress = '0x8109439D3A250f886379388CB6E29bBF64018255'
-  public static P11rAddress = '0xa13811BfdD6B8D7d28efD954Cd604ECA876f91fD'
-  public static ReserveAddress = '0x9D52198D2eb8C57363D91c2BdFC9768be6D2d191'
-  public static DatatrustAddress = '0xD9c1120FA4f001d10A6209210EF408733d07B392'
+  public static EtherTokenAddress = '0x6Ab1e58FDe8D70E68463722cc426Ebb5332ee5Aa'
+  public static MarketTokenAddress = '0x993Ee9B19e760ae7651B89C68066D656Ac81246F'
+  public static VotingAddress = '0xE778070E092798092E2a3a4E5E1e53174BD4e917'
+  public static ParameterizerAddress = '0xdAfb952371C2EFD506B920E56834ea9Fd102Ce0C'
+  public static ReserveAddress = '0xBB9e1f4D71A2d5B19D65757B452005A1a02186BC'
+  public static DatatrustAddress = '0xA451e91bdB58e1B52707B41f2af2c1774221eD0C'
+  public static ListingAddress = '0xD5df48Da838676E246e9537bB298E19B0aa00c58'
 }
