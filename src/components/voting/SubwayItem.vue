22edc8a103f8e3dfb2d6b0f1ef50669ce0256ff0
Reid Williams <reid@computable.io>
2019-10-29 10:30:30 -0700

    add fonts

--- src/components/voting/SubwayItem.vue
+++ src/components/voting/SubwayItem.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <div
-      class="subway-item-wrapper"
-      :class="subwayItemWrapperClass" >
-      <!-- insert dot icon here -->
-      <div :class="subwayItemContainerClass">
-        <slot></slot>
-      </div>
+  <div class="subway-item-container">
+    <div v-if="!isFirst" class="line"></div>
+    <div class="item">
+      <slot></slot>
     </div>
-</div>
-
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 
+import '@/assets/style/ui/subway-item.sass'
+
 @Component
 export default class SubwayItem extends Vue {
   @Prop() private isIconTop!: boolean
+  @Prop({ default: false }) private isFirst!: boolean
 
   get subwayItemWrapperClass(): string {
     return 'subway-item'.concat(this.classPrepend)
