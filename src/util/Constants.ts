818f26d36a7e2e9e4b6420e7fdfb59daa5322648
Reid Williams <reid@computable.io>
2019-10-31 15:10:21 -0700

    change text constants back

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -68,7 +68,7 @@ export const Labels = {
   POSSIBLE: 'possible',
   ACCEPT: 'Accept',
   REJECT: 'Reject',
-  THIS_IS_A_CANDIDATE: 'This is a candidate listing. Voting is open.',
+  THIS_IS_A_CANDIDATE: 'This is a candidate listing.',
   VOTING_IS_OPEN: 'Voting is open.',
 }
 
