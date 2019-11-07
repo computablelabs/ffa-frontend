5442250355d72dc1eeee8a29a7f0f6fa21b2d6c2
Melvin Mallari <melvintmallari@gmail.com>
2019-11-06 19:44:09 -0800

    Add env variables for timer

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -5,5 +5,5 @@ export const Config = {
   BlockchainNetwork: ServerAddresses.Skynet,
   BlockchainWaitTime: WaitTimes.SKYNET,
   // TaskPollingTime: 1000 * 60 * 15,
-  TaskPollingTime: 1000 * 1,
+  TaskPollingTime: Number(process.env.VUE_APP_POLLING_TIME),
 }
