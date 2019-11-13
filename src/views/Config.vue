190d471fba89b102f85b0bc16e70c01f4557dd6e
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-11-13 09:55:18 +0700

    create new /config route

---
+++ src/views/Config.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="config">
+   <h2>FFA {{ versionString }}</h2>
+   <h3>Ethereum Network: {{ networkName }}</h3>
+   <h3>JSON RPC Endpoint: {{ jsonRpcEndpoint }}</h3>
+   <h3>Datatrust URL: {{ datatrustUrl }}</h3>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
+
+@Component
+export default class Config extends Vue {
+
+  public get versionString(): string {
+    return 'v. 0.9 beta <- someone fix this'
+  }
+
+  public get networkName(): string {
+    return process.env.VUE_APP_NETWORK_NAME!
+  }
+
+  public get jsonRpcEndpoint(): string {
+    return process.env.VUE_APP_ETHEREUM_JSON_RPC_PROVIDER!
+  }
+
+  public get datatrustUrl(): string {
+    // TODO: implement
+    return 'Melvin, fix me'
+  }
+
+}
+</script>
---
+++ tests/unit/views/Config.spec.ts
@@ -0,0 +1,45 @@
+import { mount, createLocalVue, Wrapper } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+
+
+import Navigation from '../../../src/components/ui/Navigation.vue'
+import Drawer from '../../../src/components/ui/Drawer.vue'
+import Config from '../../../src/views/Config.vue'
+
+import flushPromises from 'flush-promises'
+
+// tslint:disable no-shadowed-variable
+
+const localVue = createLocalVue()
+
+let wrapper!: Wrapper<Config>
+
+const sectionId = 'config'
+
+const fakeRealAddress = '0x2C10c931FEbe8CA490A0Da3F7F78D463550CB048'
+
+describe('Config.vue', () => {
+
+  beforeAll(() => {
+    localVue.use(VueRouter)
+    localVue.component('Navigation', Navigation)
+    localVue.component('Drawer', Drawer)
+  })
+
+  afterEach(() => {
+    flushPromises()
+    wrapper.destroy()
+  })
+
+  it('renders Config', () => {
+    wrapper = mount(Config, {})
+    expect(wrapper.findAll(`#${sectionId}`).length).toBe(1)
+    expect(wrapper.findAll(`h2`).length).toBe(1)
+    expect(wrapper.find('h2').text().startsWith('FFA')).toBeTruthy()
+    const h3s = wrapper.findAll('h3')
+    expect(h3s.length).toBe(3)
+    expect(h3s.at(0).text().startsWith('Ethereum Network')).toBeTruthy()
+    expect(h3s.at(1).text().startsWith('JSON RPC')).toBeTruthy()
+    expect(h3s.at(2).text().startsWith('Datatrust')).toBeTruthy()
+  })
+})
