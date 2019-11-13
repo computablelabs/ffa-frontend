bbb1acb6360c4541131915f834cb15125b2630c5
Will Button <will@willbutton.co>
2019-11-13 13:27:33 -0700

    switch to build number

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,11 @@
 # Build and tag docker image
 docker build --build-arg HTTP_USERNAME=$HTTP_USERNAME --build-arg HTTP_PASSWORD=$HTTP_PASSWORD -t "ffa:$TRAVIS_BRANCH" .
 docker tag "ffa:$TRAVIS_BRANCH" "$REPO_URI:$TRAVIS_BRANCH"
-docker tag "ffa:$TRAVIS_BRANCH" "$REPO_URI:$TRAVIS_COMMIT"
+docker tag "ffa:$TRAVIS_BRANCH" "$REPO_URI:$TRAVIS_BUILD_NUMBER"
 $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
-echo "Pushing images as $REPO_URI:$TRAVIS_BRANCH and $REPO_URI:$TRAVIS_COMMIT"
+echo "Pushing images as $REPO_URI:$TRAVIS_BRANCH and $REPO_URI:$TRAVIS_BUILD_NUMBER"
 docker push "$REPO_URI:$TRAVIS_BRANCH"
-docker push "$REPO_URI:$TRAVIS_COMMIT"
+docker push "$REPO_URI:$TRAVIS_BUILD_NUMBER"
 
 # Force deploy service to pick up new docker image & wait for success
 echo "Restarting $SERVICE to complete deployment"
