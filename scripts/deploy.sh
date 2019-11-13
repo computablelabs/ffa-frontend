15a239c719c444f0c35806e50358e99b5084db3f
Will Button <will@willbutton.co>
2019-11-13 14:51:40 -0700

    remove conditional builds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 # Build and tag docker image
-docker build --build-arg HTTP_USERNAME=$HTTP_USERNAME --build-arg HTTP_PASSWORD=$HTTP_PASSWORD -t "ffa:skynet" .
+docker build --build-arg HTTP_USERNAME=$HTTP_USERNAME --build-arg HTTP_PASSWORD=$HTTP_PASSWORD -t "ffa:$TRAVIS_BRANCH" .
 docker tag "ffa:$TRAVIS_BRANCH" "$REPO_URI:$TRAVIS_BRANCH"
 docker tag "ffa:$TRAVIS_BRANCH" "$REPO_URI:$TRAVIS_BUILD_NUMBER"
 $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
