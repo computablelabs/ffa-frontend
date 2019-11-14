52cb4944a04fd30731bf79213f702c0ee7fd0867
MelvinMallari <36467250+MelvinMallari@users.noreply.github.com>
2019-11-13 14:06:50 -1000

    Fixes #520 - Add env variables for CAPI endpoints (#534)
    * add different capi env
    
    * Add datatrust url to config.vue

--- src/util/Servers.ts
+++ src/util/Servers.ts
@@ -1,5 +1,5 @@
 export default class Servers {
-  public static Datatrust = 'https://ffastaging.computablelabs.com'
+  public static Datatrust = process.env.VUE_APP_DATATRUST_URL
   public static EthereumJsonRpcProvider = process.env.VUE_APP_ETHEREUM_JSON_RPC_PROVIDER
   public static Coinbase = 'https://api.coinbase.com'
 }
