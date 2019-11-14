7250040eb5a48d397da5c868f294c6f90c796e89
Ken Mitsumoto <ken.mitsumoto@gmail.com>
2019-11-14 12:02:54 +0700

    Fixes #550

--- src/components/supportWithdraw/UnwrapWETHStep.vue
+++ src/components/supportWithdraw/UnwrapWETHStep.vue
@@ -1,27 +1,9 @@
 <template>
   <div class="withdraw-unwrap-weth">
-    <div class="indicator">
-      <ProcessButton
-        :buttonText="labelText"
-        :clickable="processEnabled"
-        :processing="isProcessing"
-        :onClickCallback="onClickCallback"
-        v-if="showButton"/>
-
-      <BlockchainExecutingMessage
-        v-if="showBlockchainMessage">
-        <div slot="messageSlot" class="executing-message">
-          CHANGE ME Unwrapping {{ wethValue }} WETH
-        </div>
-      </BlockchainExecutingMessage>
-
-      <DrawerMessage
-        v-if="showDrawerMessage">
-        <div slot="messageSlot" class="check-light-icon drawer-message">
-          CHANGE ME Unwrapper {{ wethValue }} WETH
-        </div>
-      </DrawerMessage>
-    </div>
+    <DrawerBlockchainStep
+      :label="drawerLabel"
+      :state="drawerStepState"
+      :onButtonClick="onClickCallback"/>
   </div>
 </template>
 
@@ -35,14 +17,13 @@ import AppModule from '../../vuexModules/AppModule'
 import SupportWithdrawModule from '../../vuexModules/SupportWithdrawModule'
 import FlashesModule from '../../vuexModules/FlashesModule'
 
-import ProcessButton from '@/components/ui/ProcessButton.vue'
-
 import { Eventable } from '../../interfaces/Eventable'
 
 import { WithdrawStep } from '../../models/WithdrawStep'
 import FfaListing from '../../models/FfaListing'
 import ContractAddresses from '../../models/ContractAddresses'
 import Flash, { FlashType } from '../../models/Flash'
+import { DrawerBlockchainStepState } from '../../models/DrawerBlockchainStepState'
 
 import EtherTokenContractModule from '../../functionModules/protocol/EtherTokenContractModule'
 import EventableModule from '../../functionModules/eventable/EventableModule'
@@ -50,23 +31,57 @@ import SupportWithdrawProcessModule from '../../functionModules/components/Suppo
 
 import { Labels, Errors } from '../../util/Constants'
 
-import BlockchainExecutingMessage from '../ui/BlockchainExecutingMessage.vue'
-import DrawerMessage from '../ui/DrawerMessage.vue'
+import DrawerBlockchainStep from '../ui/DrawerBlockchainStep.vue'
 
 import uuid4 from 'uuid/v4'
 
 @Component({
   components: {
-    ProcessButton,
-    BlockchainExecutingMessage,
-    DrawerMessage,
+    DrawerBlockchainStep,
   },
 })
 export default class UnwrapWETHStep extends Vue {
 
-  public get processEnabled(): boolean {
-    return getModule(SupportWithdrawModule, this.$store).withdrawStep === WithdrawStep.UnwrapWETH &&
-      getModule(AppModule, this.$store).etherTokenBalance > 0
+  public processId!: string
+  public unsubscribe!: () => void
+
+  public supportWithdrawModule =  getModule(SupportWithdrawModule, this.$store)
+  public flashesModule = getModule(FlashesModule, this.$store)
+
+  public get drawerLabel(): string {
+    switch (this.supportWithdrawModule.withdrawStep) {
+
+      case WithdrawStep.Error:
+      case WithdrawStep.Withdraw:
+        return `CHANGE ME ${Labels.UNWRAP_WETH}`
+
+      case WithdrawStep.WithdrawPending:
+        return `CHANGE ME ${Labels.UNWRAP_WETH}`
+
+      default:
+        return `CHANGE ME ${Labels.UNWRAP_WETH}`
+    }
+  }
+
+  public get drawerStepState(): DrawerBlockchainStepState {
+    switch (this.supportWithdrawModule.withdrawStep) {
+
+      case WithdrawStep.Error:
+      case WithdrawStep.UnwrapWETH:
+        return DrawerBlockchainStepState.ready
+
+      case WithdrawStep.CollectIncome:
+      case WithdrawStep.CollectIncomePending:
+      case WithdrawStep.Withdraw:
+      case WithdrawStep.WithdrawPending:
+        return DrawerBlockchainStepState.upcoming
+
+      case WithdrawStep.UnwrapWETHPending:
+        return DrawerBlockchainStepState.processing
+
+      default:
+        return DrawerBlockchainStepState.completed
+    }
   }
 
   public get wethValue(): number {
@@ -77,37 +92,6 @@ export default class UnwrapWETHStep extends Vue {
       this.supportWithdrawModule.withdrawValue,
       this.$store)
   }
-
-  public get isProcessing(): boolean {
-    return getModule(SupportWithdrawModule, this.$store).withdrawStep === WithdrawStep.UnwrapWETHPending
-  }
-
-  public get hasTransactionId(): boolean {
-    if (!this.supportWithdrawModule.unwrapWETHTransacctionId) {
-      return false
-    }
-    return this.supportWithdrawModule.unwrapWETHTransacctionId.length > 0
-  }
-
-  public get showButton(): boolean {
-    return !this.hasTransactionId &&
-      this.supportWithdrawModule.withdrawStep < WithdrawStep.Complete
-  }
-
-  public get showBlockchainMessage(): boolean {
-    return this.hasTransactionId &&
-      this.supportWithdrawModule.withdrawStep === WithdrawStep.UnwrapWETHPending
-  }
-
-  public get showDrawerMessage(): boolean {
-    return this.supportWithdrawModule.withdrawStep >= WithdrawStep.Complete
-  }
-  public labelText = Labels.UNWRAP_WETH
-  public processId!: string
-  public unsubscribe!: () => void
-
-  protected supportWithdrawModule =  getModule(SupportWithdrawModule, this.$store)
-  protected flashesModule = getModule(FlashesModule, this.$store)
 
   public created() {
     this.unsubscribe = this.$store.subscribe(this.vuexSubscriptions)
