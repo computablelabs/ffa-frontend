2211c74d51ed3c0c15efe7b1ea1fc1957710aeb4
Melvin Mallari <melvintmallari@gmail.com>
2019-11-26 17:13:26 -0800

    Add PurchaseDrawer specs

--- tests/unit/components/ui/PurchaseDrawer.spec.ts
+++ tests/unit/components/ui/PurchaseDrawer.spec.ts
@@ -1,16 +1,46 @@
-import { mount, createLocalVue, Wrapper } from '@vue/test-utils'
+import { shallowMount, mount, createLocalVue, Wrapper } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import { getModule } from 'vuex-module-decorators'
 import appStore from '../../../../src/store'
-import DrawerModule, { DrawerState } from '../../../../src/vuexModules/DrawerModule'
 
-import BaseDrawer from '@/views/drawers/BaseDrawer.vue'
+import FfaListingsModule from '../../../../src/vuexModules/FfaListingsModule'
+import DrawerModule from '../../../../src/vuexModules/DrawerModule'
+import PurchaseModule from '../../../../src/vuexModules/PurchaseModule'
+
+import FfaListing, { FfaListingStatus } from '../../../../src/models/FfaListing'
+
+import AppModule from '../../../../src/vuexModules/AppModule'
+
+import { Errors } from '../../../../src/util/Constants'
+
+import PurchaseDrawer from '@/views/drawers/PurchaseDrawer.vue'
+import { ProcessStatus } from '../../../../src/models/ProcessStatus'
 
 const localVue = createLocalVue()
 localVue.use(VueRouter)
 const drawerOpenClass = 'open'
+
 const drawerModule = getModule(DrawerModule, appStore)
-let wrapper!: Wrapper<BaseDrawer>
+let appModule = getModule(AppModule, appStore)
+let ffaListingsModule = getModule(FfaListingsModule, appStore)
+let purchaseModule = getModule(PurchaseModule, appStore)
+
+let wrapper!: Wrapper<PurchaseDrawer>
+
+const ffaListing = new FfaListing(
+  'title',
+  'description',
+  'type',
+  'hash',
+  'md5',
+  'license',
+  0,
+  '0xwallet',
+  ['foo', 'bar'],
+  FfaListingStatus.listed,
+  0,
+  0,
+)
 
 describe('BaseDrawer.vue', () => {
 
@@ -18,22 +48,65 @@ describe('BaseDrawer.vue', () => {
     localVue.use(VueRouter)
     drawerModule.setDrawerOpenClass(drawerOpenClass)
 
-    wrapper = mount(BaseDrawer, {
-      attachToDocument: true,
-      store: appStore,
-      localVue,
-    })
+    appModule = getModule(AppModule, appStore)
+    ffaListingsModule = getModule(FfaListingsModule, appStore)
+    purchaseModule = getModule(PurchaseModule, appStore)
   })
 
   afterAll(() => { wrapper.destroy() })
 
   describe('rendering', () => {
     it('renders correctly', () => {
-      wrapper = mount(BaseDrawer, {
+      wrapper = shallowMount(PurchaseDrawer, {
         attachToDocument: true,
         store: appStore,
         localVue,
       })
+
+      expect(wrapper.find('purchaseprocess-stub').exists()).toBeTruthy()
+    })
+  })
+
+  describe('computed properties', () => {
+    it('computes errorMessage and listing correctly', () => {
+      wrapper = shallowMount(PurchaseDrawer, {
+        attachToDocument: true,
+        store: appStore,
+        localVue,
+      })
+
+      appModule.setAppReady(false)
+      let errorMessage = getErrorMessage(wrapper)
+      expect(errorMessage).toBe(Errors.METAMASK_NOT_CONNECTED)
+
+      appModule.setAppReady(true)
+      errorMessage = getErrorMessage(wrapper)
+      expect(errorMessage).toBe(Errors.INVALID_LISTING_HASH)
+
+      // @ts-ignore
+      expect(wrapper.vm.listing).toBeUndefined()
+
+      wrapper.setProps({ listingHash: 'hash' })
+      ffaListingsModule.setListed([ffaListing])
+
+      // @ts-ignore
+      expect(wrapper.vm.listing).toBe(ffaListing)
+    })
+
+    it('computes isExecuting correctly', () => {
+      purchaseModule.setStatus(ProcessStatus.Error)
+
+      // @ts-ignore
+      expect(wrapper.vm.isExecuting).toBe(false)
+
+      purchaseModule.setStatus(ProcessStatus.Executing)
+      // @ts-ignore
+      expect(wrapper.vm.isExecuting).toBe(true)
     })
   })
 })
+
+function getErrorMessage(wrapper: Wrapper<PurchaseDrawer>): string {
+  // @ts-ignore
+  return wrapper.vm.errorMessage
+}
