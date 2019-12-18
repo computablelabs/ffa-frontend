79be4596b3364e2133ec926503762e7ad5da5b6c
Melvin Mallari <melvintmallari@gmail.com>
2019-12-17 17:52:34 -0800

    clean up main.ts imports

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import Vue from 'vue'
 import VueRouter, { Route } from 'vue-router'
-import App from './App.vue'
+import { getModule } from 'vuex-module-decorators'
+
 import { routes } from './router'
 import store from './store'
-import { MetamaskAccountChanged } from './models/Events'
+
 import SharedModule from './functionModules/components/SharedModule'
 
+import App from './App.vue'
 import Navigation from '@/components/ui/Navigation.vue'
 import Drawer from '@/components/ui/Drawer.vue'
 
 import '@/assets/style/ffa.sass'
-import { getModule } from 'vuex-module-decorators'
+
 import AppModule from '../src/vuexModules/AppModule'
+
 import { NavigationView } from '../src/models/NavigationView'
+import { MetamaskAccountChanged } from './models/Events'
 
 Vue.component('navigation', Navigation)
 Vue.component('drawer', Drawer)
